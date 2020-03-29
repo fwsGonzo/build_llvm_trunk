d2c2a5d3af0dec7524f105a6cd1b9b370e6a0a2d
Alf-André Walla <fwsgonzo@hotmail.com>
2020-03-29 15:16:34 +0200

    Use git instead of SVN

--- build_llvm.sh
+++ build_llvm.sh
@@ -1,12 +1,14 @@
-LLVM_SOURCE=$HOME/llvm/llvm_source
+LLVM_SOURCE=$PWD/llvm-project/llvm
 LLVM_INSTALL=$HOME/llvm/install
-LLVM_BUILD=$HOME/llvm/build
+LLVM_BUILD=$PWD/build
+
+PROJECTS="clang;clang-tools-extra;libcxx;libcxxabi;libunwind;lldb;compiler-rt;lld"
 
 set -e
 mkdir -p $LLVM_BUILD
 mkdir -p $LLVM_INSTALL
 pushd $LLVM_BUILD
-cmake -DCMAKE_INSTALL_PREFIX=$LLVM_INSTALL -DCMAKE_BUILD_TYPE=Release $LLVM_SOURCE/llvm
-make -j32
+cmake -DCMAKE_INSTALL_PREFIX=$LLVM_INSTALL -DLLVM_ENABLE_PROJECTS=$PROJECTS -DCMAKE_BUILD_TYPE=Release $LLVM_SOURCE
+make -j8
 make install
 popd
